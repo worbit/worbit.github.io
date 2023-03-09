821eb0eb34530b56bd4a979e04e83c20eaa9c913
worbit <mathias.bernhard@epfl.ch>
2023-03-09 13:41:14 +0100

    sdf

--- RectangleSDF/x-rectangle.js
+++ RectangleSDF/x-rectangle.js
@@ -6,7 +6,7 @@ function setup() {
   createCanvas(500, 500);
   sky = color(135, 206, 235);
   sal = color(250, 128, 114);
-  slider = createSlider(250,350,300);
+  slider = createSlider(0,100,50);
   checkbox = createCheckbox('info', false);
   img = createImage(250,250);
 }
@@ -17,7 +17,18 @@ function draw() {
 }
 
 function rectDist(x,y) {
-  return max(abs(x)-val/2,abs(y)-200/2);
+  //return max(abs(x)-val/2,abs(y)-200/2);
+  let r = 50;
+  let dx = abs(x) - (200 - r);
+  let dy = abs(y) - (150 - r);
+  let inside = max(dx, dy) - r;
+  dx = max(dx, 0);
+  dy = max(dy, 0);
+  if (inside + r > 0) {
+    return sqrt(dx * dx + dy * dy) - r;
+  } else {
+    return inside;
+  }
 }
 
 function rotate_c(cx, cy, x, y, angle) {
@@ -38,7 +49,7 @@ function calcImage() {
       var px = (i-125)*2;
       var py = (j-125)*2;
       var rp = rotate_c(0,0,px,py,-20);
-      var d = rectDist(rp[0],rp[1]);
+      var d = rectDist(rp[0],rp[1]) + val;
       // debug view
       if (checkbox.checked()) {
         img.set(i,j, color(255-abs(d)));
@@ -52,7 +63,3 @@ function calcImage() {
   }
   img.updatePixels();
 }
-
-// function keyPressed() {
-//   img.save('rect','png');
-// }
