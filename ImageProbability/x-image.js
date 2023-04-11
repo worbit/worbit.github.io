8347437dbfb7d33305ebd3cd89704c128004ab04
worbit <mathias.bernhard@epfl.ch>
2023-04-11 12:26:17 +0200

    image prob distance

--- ImageProbability/x-image.js
+++ ImageProbability/x-image.js
@@ -32,14 +32,29 @@ function setup() {
 
 function draw() {
     background(0);
-    let val = slider.value();
+    let val = slider.value()/100.0;
     //pim.loadPixels();
     randomSeed(658);
 
+    /*
+    slider 0 - 1
+    0 : chances of switch = 0
+    1 : chances = 100
+    0.5 : chances = 50 everywhere
+
+    distance 1 - 0 - 1
+    0 : change first
+    1 : change last, only high probability
+    */
+
     for (let i = 0; i < pim.width; i++) {
         for (let j = 0; j < pim.height; j++) {
-            let dsq = sq(i-pim.width/2) + sq(j-pim.height/2)
-            let r = val/100.0>random();
+            // max 31'250, half: 15'625 (125^2)
+            // let dsq = sq(i-pim.width/2) + sq(j-pim.height/2);
+            // let mod = abs(dsq-4900)/4900;
+            let dice = random();
+            // r is some combination of dist mod, val and dice
+            let r = val > dice;
             let c1 = sky;
             let c2 = sal;
             if (!r) {
