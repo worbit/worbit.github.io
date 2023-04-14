1be3c40af245e4b39f4d935dd62756a14d462d24
worbit <mathias.bernhard@epfl.ch>
2023-04-14 13:48:34 +0200

    superellipse debug view

--- Superellipse/x-ellipse.js
+++ Superellipse/x-ellipse.js
@@ -29,8 +29,8 @@ function draw() {
   translate(width / 2, height / 2);
   rotate(-1/9*PI);
 
-  let n = slider.value();
-  let na = 2 / n;
+  let val = slider.value();
+  let na = 2 / val;
 
   beginShape();
   for (let angle = 0; angle < TWO_PI; angle += 0.1) {
@@ -44,6 +44,27 @@ function draw() {
     vertex(x, y);
   }
   endShape(CLOSE);
+
+  if (checkbox.checked()) {
+    background(sky);
+    noFill();
+
+    for (let i=0; i<255; i++) {
+      stroke(i);
+      let v = (i+0.1)**0.6;
+      let na = 2/v;
+      beginShape();
+      for (let angle = 0; angle < TWO_PI; angle += 0.1) {
+        let x = pow(abs(cos(angle)), na) * a * sgn(cos(angle));
+        let y = pow(abs(sin(angle)), na) * b * sgn(sin(angle));
+        vertex(x, y);
+      }
+      endShape(CLOSE);
+    }
+
+    noStroke();
+    fill(sal);
+  }
 }
 
 function sgn(val) {
