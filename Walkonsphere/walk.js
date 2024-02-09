32a3fa1c19485273b98e1495a991f43e271fb2b9
worbit <bernhard@arch.ethz.ch>
2024-02-09 19:28:07 +0100

    tennis

---
+++ Walkonsphere/walk.js
@@ -0,0 +1,26 @@
+let r;
+
+let img;
+function preload() {
+  img = loadImage('rWyDiR.jpg');
+}
+
+function setup() {
+  createCanvas(500, 500, WEBGL);
+  r = 0;
+  fill('salmon');
+  noStroke();
+}
+
+function draw() {
+  background('skyblue');
+  translate(0,150,0);
+  if (r<PI/2) {
+    rotateY(r);
+  } else {
+    rotateX(r);
+  }
+  texture(img);
+  sphere(200);
+  r += 0.01;
+}
