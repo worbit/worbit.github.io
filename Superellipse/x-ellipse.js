2ab9ca76f3c5521ff9fc5cfc6b806d9fc177d456
worbit <bernhard@arch.ethz.ch>
2024-03-24 18:04:45 +0100

    superell debug

--- Superellipse/x-ellipse.js
+++ Superellipse/x-ellipse.js
@@ -27,7 +27,7 @@ function draw() {
   rotate(-1/9*PI);
 
   let val = slider.value();
-  let na = 2 / val;
+  let na = 1 / val;
 
   beginShape();
   for (let angle = 0; angle < TWO_PI; angle += 0.1) {
@@ -45,11 +45,12 @@ function draw() {
   if (checkbox.checked()) {
     background(sky);
     noFill();
+    stroke(0);
 
-    for (let i=0; i<255; i++) {
-      stroke(i);
-      let v = (i+0.1)**0.6;
-      let na = 2/v;
+    for (let i=0; i<400; i++) {
+      //stroke(i);
+      let v = (i+0.1)**0.9;
+      let na = 6/v;
       beginShape();
       for (let angle = 0; angle < TWO_PI; angle += 0.1) {
         let x = pow(abs(cos(angle)), na) * a * sgn(cos(angle));
